d0d2e772e51eec717805cf00b2d5bd75ab5a8ade
Piotr Leniec <leniec.piotr@gmail.com>
2017-08-24 20:25:27 +0200

    tasks index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,7 @@
 class TasksController < ApplicationController
+  def index
+  end
+
   def select_an_element_in_the_page_to_inspect_it
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
   get 'select-an-element-in-the-page-to-inspect-it',
       to: 'tasks#select_an_element_in_the_page_to_inspect_it'
   get 'toggle-device-toolbar', to: 'tasks#toggle_device_toolbar'
